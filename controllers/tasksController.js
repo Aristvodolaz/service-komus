0d234a3e342fea70058abb3b1015dc1602316558
prokopenko.a <prokopenko.a@promail366.com>
2024-07-09 15:25:34 +0300

    rout on search shk, change status, search artikul

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -1,12 +1,13 @@
 //получение артикулов по названию задания
 const mssql = require('mssql');
-const {connectToDatabase, sql } = require('../dbConfig')
+const {connectToDatabase, sql } = require('../dbConfig');
+const { error } = require('winston');
 
 const getArticulsByTaskNumber = async (req, res) => {
   const { taskNumber } = req.query;
 
   try {
-    let pool = await connectToDatabase();
+    const pool = await connectToDatabase();
     if (!pool) {
       throw new Error('Ошибка подключения к базе данных');
     }
@@ -48,7 +49,74 @@ const getUniqueTaskNames = async (req, res) => {
   }
 };
 
+const getByShk = async (req, res) => {
+  const { taskName, shk } = req.query;
+
+  try {
+    const pool = await connectToDatabase();
+    if (!pool) {
+      throw new Error('Ошибка подключения к базе данных');
+    }
+
+    const result = await pool.request()
+      .input('Nazvanie_Zadaniya', mssql.NVarChar(255), taskName)
+      .input('SHK', mssql.NVarChar(50), `%${shk}%`)
+      .query('SELECT * FROM Test_MPP WHERE SHK LIKE @SHK AND Nazvanie_Zadaniya = @Nazvanie_Zadaniya ');
+
+    res.json(result.recordset);
+  } catch (error) {
+    console.error('Ошибка при получении данных по SHK:', error);
+    res.status(500).json({ error: 'Ошибка при получении данных по SHK' });
+  }
+};
+
+const updateStatus = async (req, res) => {
+  const { taskName, shk, status } = req.body;
+
+  try {
+    const pool = await connectToDatabase();
+    if (!pool) {
+      throw new Error('Ошибка подключения к базе данных');
+    }
+
+    await pool.request()
+      .input('Nazvanie_Zadaniya', mssql.NVarChar(255), taskName)
+      .input('SHK', mssql.NVarChar(50), `%${shk}%`)
+      .input('Status', mssql.Int, status)
+      .query('UPDATE Test_MPP SET Status = @Status WHERE Nazvanie_Zadaniya = @Nazvanie_Zadaniya AND SHK LIKE @SHK');
+
+    res.json({ message: 'Статус успешно обновлен' });
+  } catch (error) {
+    console.error('Ошибка при обновлении статуса:', error);
+    res.status(500).json({ error: 'Ошибка при обновлении статуса' });
+  }
+};
+
+const getRecordsByArticul = async (req, res) => {
+  const { taskName, articul } = req.query;
+
+  try {
+    const pool = await connectToDatabase();
+    if (!pool) {
+      throw new Error('Ошибка подключения к базе данных');
+    }
+
+    const result = await pool.request()
+      .input('Nazvanie_Zadaniya', mssql.NVarChar(255), taskName)
+      .input('Artikul', mssql.Int, articul)
+      .query('SELECT * FROM Test_MPP WHERE Artikul = @Artikul AND Nazvanie_Zadaniya = @Nazvanie_Zadaniya');
+
+    res.json(result.recordset);
+  } catch (error) {
+    console.error('Ошибка при получении записей по артикулу:', error);
+    res.status(500).json({ error: 'Ошибка при получении записей по артикулу' });
+  }
+};
+
 module.exports = {
   getArticulsByTaskNumber,
-  getUniqueTaskNames
+  getUniqueTaskNames,
+  getByShk,
+  updateStatus,
+  getRecordsByArticul
 };
--- routes/market_place/tasks.js
+++ routes/market_place/tasks.js
@@ -4,5 +4,7 @@ const dataController = require('../../controllers/tasksController');
 
 router.get('/', dataController.getArticulsByTaskNumber);
 router.get('/names', dataController.getUniqueTaskNames);
-
+router.get('/searchShk', dataController.getByShk); 
+router.put('/updateStatus', dataController.updateStatus);
+router.get('/serchArticulTask', dataController.getRecordsByArticul)
 module.exports = router;
