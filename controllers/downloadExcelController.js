d0de8d0f581fd13ba14b44d71ce795abedac0ee2
prokopenko.a <prokopenko.a@promail366.com>
2024-07-29 16:58:57 +0300

    up

--- controllers/downloadExcelController.js
+++ controllers/downloadExcelController.js
@@ -3,11 +3,21 @@ const path = require('path');
 const fs = require('fs');
 const SftpClient = require('ssh2-sftp-client');
 const multer = require('multer'); // Для обработки multipart/form-data
-const { connectToDatabase } = require('../dbConfig'); // Подключение к базе данных
 
 const router = express.Router();
 const sftp = new SftpClient();
-const upload = multer({ dest: 'uploads/' }); // Папка для временного хранения загруженных файлов
+
+// Настройка хранения файлов
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); // Папка для временного хранения загруженных файлов
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname); // Используем оригинальное имя файла
+  }
+});
+
+const upload = multer({ storage: storage });
 
 // Функция для загрузки файла на сервер SFTP
 const uploadFile = async (serverInfo, localFilePath, remoteFilePath) => {
@@ -25,7 +35,12 @@ const uploadFile = async (serverInfo, localFilePath, remoteFilePath) => {
 
 // Маршрут для загрузки файла на SFTP сервер
 router.post('/upload-file', upload.single('file'), async (req, res) => {
-  const localFilePath = req.file.path; // Путь к временно сохраненному файлу
+  // Проверяем, что файл был загружен
+  if (!req.file) {
+    return res.status(400).json({ success: false, message: 'No file uploaded.' });
+  }
+
+  const localFilePath = path.join(__dirname, '..', 'uploads', req.file.originalname); // Путь к временно сохраненному файлу
   const remoteFilePath = `/root/task_file/wait/${req.file.originalname}`; // Путь для хранения файла на сервере SFTP
 
   const serverInfo = {
@@ -36,13 +51,8 @@ router.post('/upload-file', upload.single('file'), async (req, res) => {
   };
 
   try {
-    // Загрузка файла на сервер SFTP
     await uploadFile(serverInfo, localFilePath, remoteFilePath);
-    
-    // Удаление временного файла после загрузки
-    fs.unlinkSync(localFilePath);
-
-    // Отправка успешного ответа клиенту
+    fs.unlinkSync(localFilePath); // Удаление временного файла после загрузки
     res.json({ success: true, message: `Файл ${req.file.originalname} успешно загружен на SFTP сервер.` });
   } catch (error) {
     console.error(`Ошибка при загрузке файла на SFTP сервер: ${error.message}`);
