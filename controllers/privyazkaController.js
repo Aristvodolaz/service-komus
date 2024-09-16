bbcbf67105695727b74915407d64544b1c64b6e9
prokopenko.a <kap.moral@mail.ru>
2024-09-16 11:10:01 +0300

    add pruivyazka

--- controllers/privyazkaController.js
+++ controllers/privyazkaController.js
@@ -3,7 +3,7 @@ const { connectToDatabase, sql } = require('../dbConfig');
 const { error } = require('winston');
 
 const addZapis = async (req, res) => {
-    const { name, artikul, kolvo, pallet, shk} = req.body;  // You may need to capture additional fields required for insertion
+    const { name, artikul, kolvo, pallet, shk} = req.body;  
   
     try {
         const pool = await connectToDatabase();
@@ -30,6 +30,61 @@ const addZapis = async (req, res) => {
     }
 };
 
+
+const getZapis = async (req, res) => {
+    const { name, artikul} = req.body;  
+
+    try {
+        const pool = await connectToDatabase();
+        if (!pool) {
+            return res.status(500).json({ success: false, value: null, errorCode: 500 });
+        }
+
+        // Выполнение SELECT-запроса для получения всех записей с нужными полями
+        await pool.request()
+            .input('Nazvanie_Zadaniya', mssql.NVarChar(255), name)
+            .input('Artikul', mssql.Int, artikul)
+            .query(`
+                SELECT Nazvanie_Zadaniya, Artikul, Srok_Godnosti, SHK_WPS, Pallet_No, Kolvo_Tovarov
+                FROM Test_MP_Privyazka
+            `);
+
+        res.json({ success: true, value: result.recordset, errorCode: 200 });
+    } catch (error) {
+        console.error('Ошибка при получении записей:', error);
+        res.status(500).json({ success: false, value: null, errorCode: 500 });
+    }
+};
+
+
+const addSrokGodnosti = async (req, res) => {
+    const { name, artikul, srok_godnosti} = req.body;  
+  
+    try {
+        const pool = await connectToDatabase();
+        if (!pool) {
+            return res.status(500).json({ success: false, value: null, errorCode: 500 });
+        }
+  
+        // Добавление новой записи
+        await pool.request()
+            .input('Nazvanie_Zadaniya', mssql.NVarChar(255), name)
+            .input('Artikul', mssql.Int, artikul)
+            .input('Srok_Godnosti', mssql.NVarChar(255), srok_godnosti)
+            .query(`
+                INSERT INTO Test_MP_Privyazka (Nazvanie_Zadaniya, Artikul, Srok_Godnosti)
+                VALUES (@Nazvanie_Zadaniya, @Artikul, @Srok_Godnosti)
+            `);
+  
+        res.json({ success: true, value: 'Запись успешно добавлена', errorCode: 200 });
+    } catch (error) {
+        console.error('Ошибка при добавлении записи:', error);
+        res.status(500).json({ success: false, value: null, errorCode: 500 });
+    }
+};
+
 module.exports = {
-    addZapis
+    addZapis,
+    addSrokGodnosti,
+    getZapis
 };
--- routes/privyazka.js
+++ routes/privyazka.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const dataController = require('../controllers/privyazkaController');
 
 router.post('/add', dataController.addZapis);
+router.get('/getZapis', dataController.getZapis)
+router.post('/addSrokGodnosti', dataController.addSrokGodnosti)
 
 module.exports = router;
