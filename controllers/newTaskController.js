8c715d5b96e7b31cf3c49ab65cefe465554737ab
prokopenko.a <kap.moral@mail.ru>
2024-12-24 10:18:55 +0300

    минор

--- controllers/newTaskController.js
+++ controllers/newTaskController.js
@@ -126,6 +126,7 @@ const updateStatusNew = async (req, res) => {
         .input('Sht_v_MP', mssql.Int, originalRecord.Sht_v_MP)
         .input('Itog_MP', mssql.Int, originalRecord.Itog_MP)
         .input('SOH', mssql.NVarChar(10), originalRecord.SOH)
+        .input('Scklad_Pref', mssql.mssql.NVarChar(50), originalRecord.Scklad_Pref)
         .input('Tip_Postavki', mssql.NVarChar(50), originalRecord.Tip_Postavki)
         .input('Srok_Godnosti', mssql.NVarChar(50), originalRecord.Srok_Godnosti)
         .input('Op_1_Bl_1_Sht', mssql.NVarChar(10), originalRecord.Op_1_Bl_1_Sht)
@@ -167,7 +168,7 @@ const updateStatusNew = async (req, res) => {
         .query(`
           INSERT INTO Test_MP (
             Pref, Nazvanie_Zadaniya, Status_Zadaniya, Status, Ispolnitel, Artikul, Artikul_Syrya, 
-            Nomenklatura, Nazvanie_Tovara, SHK, SHK_SPO, SHK_SPO_1, Kol_vo_Syrya, Itog_Zakaz, 
+            Nomenklatura, Nazvanie_Tovara, SHK, SHK_SPO, SHK_SPO_1, Kol_vo_Syrya, Itog_Zakaz, Scklad_Pref,
             Sht_v_MP, Itog_MP, SOH, Tip_Postavki, Srok_Godnosti, Op_1_Bl_1_Sht, Op_2_Bl_2_Sht, 
             Op_3_Bl_3_Sht, Op_4_Bl_4_Sht, Op_5_Bl_5_Sht, Op_6_Blis_6_10_Sht, Op_7_Pereschyot, 
             Op_9_Fasovka_Sborka, Op_10_Markirovka_SHT, Op_11_Markirovka_Prom, Op_12_Markirovka_Prom, 
@@ -181,7 +182,7 @@ const updateStatusNew = async (req, res) => {
             @Nomenklatura, @Nazvanie_Tovara, @SHK, @SHK_SPO, @SHK_SPO_1, @Kol_vo_Syrya, @Itog_Zakaz, 
             @Sht_v_MP, @Itog_MP, @SOH, @Tip_Postavki, @Srok_Godnosti, @Op_1_Bl_1_Sht, @Op_2_Bl_2_Sht, 
             @Op_3_Bl_3_Sht, @Op_4_Bl_4_Sht, @Op_5_Bl_5_Sht, @Op_6_Blis_6_10_Sht, @Op_7_Pereschyot, 
-            @Op_9_Fasovka_Sborka, @Op_10_Markirovka_SHT, @Op_11_Markirovka_Prom, @Op_12_Markirovka_Prom, 
+            @Op_9_Fasovka_Sborka, @Op_10_Markirovka_SHT, @Op_11_Markirovka_Prom, @Op_12_Markirovka_Prom,@Scklad_Pref, 
             @Op_13_Markirovka_Fabr, @Op_14_TU_1_Sht, @Op_15_TU_2_Sht, @Op_16_TU_3_5, @Op_17_TU_6_8, 
             @Op_468_Proverka_SHK, @Op_469_Spetsifikatsiya_TM, @Op_470_Dop_Upakovka, @Sortiruemyi_Tovar, 
             @Ne_Sortiruemyi_Tovar, @Produkty, @Opasnyi_Tovar, @Zakrytaya_Zona, @Krupnogabaritnyi_Tovar, 
