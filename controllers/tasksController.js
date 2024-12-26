b030b83b446a013566db1c7a177be4a806154cd7
prokopenko.a <kap.moral@mail.ru>
2024-12-26 18:49:53 +0300

    minor

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -240,13 +240,11 @@ const getRecordsByArticul = async (req, res) => {
     console.error('Ошибка при получении записей по артикулу:', error);
     res.status(500).json({ success: false, value: null, errorCode: 500 });
   }
-};
+};const deleteRecords = async (req, res) => {
+  const { id, task } = req.query;
 
-const deleteRecords = async (req, res) => {
-  const { id , task} = req.query;
-
-  if (!id || !task ) {
-    return res.status(400).json({ success: false, value: 'id, task are required', errorCode: 400 });
+  if (!id || !task) {
+    return res.status(400).json({ success: false, value: 'id and task are required', errorCode: 400 });
   }
 
   try {
@@ -254,23 +252,26 @@ const deleteRecords = async (req, res) => {
     if (!pool) {
       throw new Error('Ошибка подключения к базе данных');
     }
-    const tableName = task.includes('WB') ? 'Test_MP_Privyazka' : 'Test_MP';
-    let result;
 
-    if(tableName){ result = await pool.request()
+    // Выбор таблицы на основе условия
+    const tableName = task.includes('WB') ? 'Test_MP_Privyazka' : 'Test_MP';
+
+    // Удаление элемента из выбранной таблицы
+    const result = await pool.request()
       .input('ID', mssql.BigInt, id)
-      .query('Delete FROM Test_MP_Privyazka WHERE ID = @id');
-    } else{
-      const result = await pool.request()
-      .input('ID', mssql.BigInt, id)
-      .query('Delete FROM Test_MP WHERE ID = @id');
+      .query(`DELETE FROM ${tableName} WHERE ID = @ID`);
+
+    if (result.rowsAffected[0] > 0) {
+      res.status(200).json({ success: true, value: null, errorCode: 200 });
+    } else {
+      res.status(404).json({ success: false, value: 'Record not found', errorCode: 404 });
     }
-    res.status(200).json({ success: true, value: result.recordset, errorCode: 200 });
   } catch (error) {
-    console.error('Ошибка при получении записей по артикулу:', error);
+    console.error('Ошибка при удалении записи:', error);
     res.status(500).json({ success: false, value: null, errorCode: 500 });
   }
 };
+
 
 const getRecordsByWPS = async (req, res) => {
   const { shk } = req.query;
