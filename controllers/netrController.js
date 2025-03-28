5fbc747549ca41c4d5114ab5a6eda0612ed5387d
prokopenko.a <kap.moral22@gmail.com>
2025-03-28 14:51:32 +0300

    minor

--- controllers/netrController.js
+++ controllers/netrController.js
@@ -361,8 +361,9 @@ async function distinctName(req, res) {
         }
 
         const query = `
-            SELECT DISTINCT nazvanie_zdaniya FROM x_Packer_Netr
-            ORDER BY nazvanie_zdaniya
+            SELECT DISTINCT Nazvanie_Zadaniya FROM Test_MP
+            where Scklad_Pref = 'NETR' and Status_Zadaniya = 0
+            ORDER BY Nazvanie_Zadaniya
         `;
 
         const request = pool.request();
